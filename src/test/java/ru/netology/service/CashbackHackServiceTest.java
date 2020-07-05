2251ea8521b70db7f6d581d3bf755d378def6325
Alla Alenova <alla_alenova@mail.ru>
2020-07-05 18:23:58 +0300

    fixed bag

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
         int amount = 10000;
         int actual = cashbackHackService.remain(amount);
         int expected = 1000;
-        assertEquals (actual,expected);
+        assertEquals (expected, actual);
 
     }
 
@@ -23,7 +23,7 @@ public class CashbackHackServiceTest {
         int amount = 900;
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
-        assertEquals (actual,expected);
+        assertEquals (expected, actual);
 
 
     }
@@ -34,7 +34,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals (actual,expected);
+        assertEquals (expected, actual);
 
     }
 
